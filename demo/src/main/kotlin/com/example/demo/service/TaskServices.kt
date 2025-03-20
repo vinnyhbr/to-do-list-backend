093886cd2506d9f3a7778637f4c394d91c552336
vinnyhbr <113705995+vinnyhbr@users.noreply.github.com>
2025-03-20 10:25:57 -0300

    feat: criando a classe service

--- demo/src/main/kotlin/com/example/demo/service/TaskServices.kt
+++ demo/src/main/kotlin/com/example/demo/service/TaskServices.kt
@@ -1,7 +1,27 @@
 package com.example.demo.service
 
+import com.example.demo.model.Task
+import com.example.demo.repository.TaskRepository
+import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
-class TaskServices {
+class TaskService(private val repository: TaskRepository) {
+    fun getAllTasks(): List<Task> = repository.findAll()
+
+    fun createTask(task: Task): Task = repository.save(task)
+
+    fun getTaskById(id: Long): Task = repository.findById(id).orElseThrow { RuntimeException("Task not found") }
+
+    @Transactional
+    fun updateTask(id: Long, updatedTask: Task): Task {
+        return repository.findById(id).map {
+            val task = it.copy(title = updatedTask.title, description = updatedTask.description, completed = updatedTask.completed)
+            repository.save(task)
+        }.orElseThrow { RuntimeException("Task not found") }
+    }
+
+    fun deleteTask(id: Long) {
+        repository.deleteById(id)
+    }
 }
